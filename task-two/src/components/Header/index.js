ef06e33cf3d487ff256ea7810661969d860161c6
musanabiyev <musanebiyev@gmail.com>
2022-03-18 19:41:27 +0400

    task two

---
+++ task-two/src/components/Header/index.js
@@ -0,0 +1,71 @@
+
+import { useState } from 'react';
+
+import './todos.css'
+
+import Section from './Section';
+import Footer from './Footer';
+
+function Todos() {
+
+    const [todos, setTodos] = useState([
+        {
+            todolist: "Learn JavaScript"
+        }, {
+            todolist: "Learn React"
+        }, {
+            todolist: "Have a life!"
+        }, {
+            todolist: "Fall in love..."
+        },
+    ])
+
+    const [activeTodos, setActiveTodos] = useState("All");
+
+    const removeTodos = (todolist) => {
+        const filtered = todos.filter(item => item.todolist !== todolist)
+        setTodos(filtered);
+    }
+
+
+    let filteredTodos =
+        activeTodos === "All" ? todos
+            : activeTodos === "Active" ? todos.filter(item => item.complete === false)
+                : todos.filter(item => item.complete === true)
+
+
+    const updateCompleteTodos = (todolist) => {
+        const index = todos.findIndex(item => item.todolist === todolist);
+        const todo = todos.find(item => item.todolist === todolist);
+        todo.complete = !todo.complete
+        const newTodos = [...todos];
+        newTodos[index] = todo;
+        setTodos(newTodos);
+    }
+
+
+
+    return (
+
+        <div id="todoapp">
+
+            <Section
+                addTodos={setTodos}
+                todos={todos}
+            />
+
+            <Footer
+                updateCompleteTodos={updateCompleteTodos}
+                activeTodos={activeTodos}
+                setActiveTodos={setActiveTodos}
+                removeTodos={removeTodos}
+                filteredTodos={filteredTodos}
+                addTodos={setTodos}
+                todos={todos}
+            />
+
+        </div>
+    )
+}
+
+export default Todos;
